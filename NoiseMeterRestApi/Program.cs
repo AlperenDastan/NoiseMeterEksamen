d9f456bffe94928d5e280fa3f6ab1745a45729db
OguzDastan <33421040+OguzDastan@users.noreply.github.com>
2023-12-17 03:38:26 +0100

    fixed deployedment
    Swagger was configured for Development, so it didnt show up

--- NoiseMeterRestApi/Program.cs
+++ NoiseMeterRestApi/Program.cs
@@ -8,11 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<InMemoryDBContext>(o => 
+builder.Services.AddDbContext<InMemoryDBContext>(o =>
 {
     o.UseInMemoryDatabase("NoiseMeters");
-    
-
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,11 +19,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+// Enable Swagger in non-development environments for testing purposes. 
+// Remember to restrict this in production for security reasons.
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
